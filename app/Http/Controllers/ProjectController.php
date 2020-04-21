742320fb9f3b108ed8beb94225270de23fb0b874
Bryan <andre-bryan01@outlook.com>
2020-04-21 15:27:21 +0200

    web php modified routes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,8 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        $projects = Project::all();
+        return view('welcome',compact('projects'));
     }
 
     /**
